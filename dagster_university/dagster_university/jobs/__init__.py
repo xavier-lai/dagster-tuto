4681571385d5492cf7ff4bcf44a16e9274653d51
Xavier Lai <xavier.l@cabaia.fr>
2024-10-03 19:53:54 +0200

    import in init

--- dagster_university/dagster_university/jobs/__init__.py
+++ dagster_university/dagster_university/jobs/__init__.py
@@ -4,6 +4,6 @@ trips_by_week = AssetSelection.assets("trips_by_week")
 trip_update_job = define_asset_job(
     name="trip_update_job", selection=AssetSelection.all() - trips_by_week
 )
-weekly_updated_trip_job = define_asset_job(
-    name="weekly_update_job", selection=trips_by_week
+weekly_trip_update_job = define_asset_job(
+    name="weekly_trip_update_job", selection=trips_by_week
 )

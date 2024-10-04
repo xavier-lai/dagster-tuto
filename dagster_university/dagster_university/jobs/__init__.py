72aa5ad7f2e707b9e12a7075ac62a1ec3210d3db
Xavier Lai <xavier.l@cabaia.fr>
2024-10-04 15:26:36 +0200

    job  use partition too

--- dagster_university/dagster_university/jobs/__init__.py
+++ dagster_university/dagster_university/jobs/__init__.py
@@ -1,9 +1,14 @@
 from dagster import AssetSelection, define_asset_job
+from ..partitions import monthly_partition, weekly_partition
 
 trips_by_week = AssetSelection.assets("trips_by_week")
 trip_update_job = define_asset_job(
-    name="trip_update_job", selection=AssetSelection.all() - trips_by_week
+    partitions_def=monthly_partition,
+    name="trip_update_job",
+    selection=AssetSelection.all() - trips_by_week,
 )
 weekly_trip_update_job = define_asset_job(
-    name="weekly_trip_update_job", selection=trips_by_week
+    partitions_def=weekly_partition,
+    name="weekly_trip_update_job",
+    selection=trips_by_week,
 )

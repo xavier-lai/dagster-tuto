a16036aaf20e870048bc9abb83aa0aeb1e38b2bb
Xavier Lai <xavier.l@cabaia.fr>
2024-10-18 18:57:57 +0200

    overwrite dbt models configs

--- dagster-and-dbt/dagster_university/assets/dbt.py
+++ dagster-and-dbt/dagster_university/assets/dbt.py
@@ -1,11 +1,24 @@
-from dagster import AssetExecutionContext
-from dagster_dbt import dbt_assets, DbtCliResource
+from dagster import AssetExecutionContext, AssetKey
+from dagster_dbt import dbt_assets, DbtCliResource, DagsterDbtTranslator
 
 from ..project import dbt_project
 
 
+class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_asset_key(self, dbt_resource_props):
+        resource_type = dbt_resource_props["resource_type"]
+        name = dbt_resource_props["name"]
+        print("DEBUUUUUUUG", name)
+
+        if resource_type == "source":
+            return AssetKey(f"taxi_{name}")
+        else:
+            return super().get_asset_key(dbt_resource_props)
+
+
 @dbt_assets(
     manifest=dbt_project.manifest_path,
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator,
 )
 def dbt_analytics(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
